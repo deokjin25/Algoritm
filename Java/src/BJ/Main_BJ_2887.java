867d9103146c08ab7ca8058474d2b3435c97dbcd
kkomeang <ghaehfdl@naver.com>
2024-11-06 11:48:21 +0900

    1106

--- Java/src/BJ/Main_BJ_2887.java
+++ Java/src/BJ/Main_BJ_2887.java
@@ -1,82 +1,73 @@
 package BJ;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main_BJ_2887 {
-    static int N;
-    static int[] parent;
-    static List<Edge> edges = new ArrayList<>();
-    
-    static class Edge implements Comparable<Edge> {
-        int from, to, weight;
-        public Edge(int from, int to, int weight) {
-            this.from = from;
-            this.to = to;
-            this.weight = weight;
-        }
-        @Override
-        public int compareTo(Edge o) {
-            return Integer.compare(this.weight, o.weight);
-        }
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        
-        int[][] coords = new int[N][4];  // x, y, z, index
-        
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            coords[i][0] = Integer.parseInt(st.nextToken());
-            coords[i][1] = Integer.parseInt(st.nextToken());
-            coords[i][2] = Integer.parseInt(st.nextToken());
-            coords[i][3] = i;  // index
-        }
-        
-        // 각 좌표축별로 정렬하여 인접한 점들 간의 거리로 간선 추가
-        for (int d = 0; d < 3; d++) {
-            final int dim = d;
-            Arrays.sort(coords, Comparator.comparingInt(a -> a[dim]));
-            for (int i = 0; i < N - 1; i++) {
-                int dist = Math.abs(coords[i][dim] - coords[i + 1][dim]);
-                edges.add(new Edge(coords[i][3], coords[i + 1][3], dist));
-            }
-        }
-        
-        // Kruskal's MST 알고리즘 시작
-        Collections.sort(edges);
-        parent = new int[N];
-        for (int i = 0; i < N; i++) parent[i] = i;
-        
-        int totalWeight = 0;
-        int edgeCount = 0;
-        
-        for (Edge edge : edges) {
-            if (union(edge.from, edge.to)) {
-                totalWeight += edge.weight;
-                edgeCount++;
-                if (edgeCount == N - 1) break;
-            }
-        }
-        
-        System.out.println(totalWeight);
-    }
-    
-    // Find-Union functions
-    static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-    
-    static boolean union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-        if (rootA != rootB) {
-            parent[rootB] = rootA;
-            return true;
-        }
-        return false;
-    }
+	static int[] parents;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] planet = new int[N][4];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			planet[i][0] = Integer.parseInt(st.nextToken());
+			planet[i][1] = Integer.parseInt(st.nextToken());
+			planet[i][2] = Integer.parseInt(st.nextToken());
+			planet[i][3] = i;
+		}
+		
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
+		for (int i = 0; i < 3; i++) {
+			final int dim = i;
+			Arrays.sort(planet, (o1, o2) -> Integer.compare(o1[dim], o2[dim]));
+			for (int j = 0; j < N-1; j++) {
+				int turnnel = Math.abs(planet[j][dim] - planet[j+1][dim]);
+				pq.offer(new int[] {planet[j][3], planet[j+1][3], turnnel});
+			}
+		}
+		
+		parents = new int[N];
+		for (int i = 0; i < N; i++) {
+			parents[i] = i;
+		}
+		
+		int ans = 0;
+		int cnt = 0;
+		while(!pq.isEmpty()) {
+			int[] curr = pq.poll();
+			int from = curr[0];
+			int to = curr[1];
+			int cost = curr[2];
+			
+			if(union(from, to)) {
+				ans += cost;
+				if(++cnt == N-1) break;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+	private static boolean union(int from, int to) {
+		int a = findSet(from);
+		int b = findSet(to);
+		
+		if(a != b) {
+			parents[b] = a;
+			return true;
+		}
+		
+		return false;
+	}
+	private static int findSet(int x) {
+		if(parents[x] == x) return x;
+		else return parents[x] = findSet(parents[x]);
+	}
+
 }
